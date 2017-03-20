8a8fac77831016adffe8ded356697013b0f32ff2
dgwp-projects <zayneabraham1@gmail.com>
2017-03-20 16:59:06 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+	var counter = 0
+
+	function addBump(){
+		return counter++
+	}
+	
+	
+	function getBumps(){
+		return counter
+	}
+
+	return{
+		addBump,
+		getBumps
+	}
+
+}
+
+
+
+function createAnimal(animalType){
+	return function(deadlyDevice){
+		return { animalType, deadlyDevice }
+	}
+}
+
+
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
